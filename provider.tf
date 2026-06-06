5133a6e329ed3700b3dc24c8fd612569f111284d
kishore Kothapalli <kishore.kothapalli97@gmail.com>
2026-06-06 15:49:00 +0530

    terraform

--- provider.tf
+++ provider.tf
@@ -2,12 +2,19 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "6.0.0"
+      version = "6.40.0"
     }
   }
+   backend "s3" {
+    bucket = "s3-native-locking"
+    key    = "remote-state-s3nativelocking"
+    region = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+   
+   }
 }
 
 provider "aws" {
   # Configuration options
-  region = "us-east-1"
 }
